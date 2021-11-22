ac410f971ba27d5611d1e6736cf8f70abeef75c3
NawalCheema <75502031+NawalCheema@users.noreply.github.com>
2021-11-22 10:32:23 -0600

    confused code bailing out

--- catvdog.cpp
+++ catvdog.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 class Person {
         public:
+				int index;
                 int keep;
                 int leave;
-}
+        void setValues (int x, int y, int z) {
+			index = x;
+			keep = y;
+			leave = z;
+		}
+};
+
+map<bool, bool> map;
+vector <int> matching;
 
 int main() {
         // Defining variables and taking input
@@ -15,7 +25,7 @@ int main() {
         int cats;
         int dogs;
         int voters;
-        vector <int> people;
+        vector <Person> people;
 
         cin >> numTests;
 
@@ -23,31 +33,80 @@ int main() {
                 cin >> cats;
                 cin >> dogs;
                 cin >> voters;
-
+				
+				//reading votes
                 for (int k = 0; k < voters; k++){
-                        String inputString1;
-                        String inputString2;
+                        string inputString1;
+                        string inputString2;
                         cin >> inputString1;
                         cin >> inputString2;
                         int keep = std::stoi(inputString1.at(0));
-                        int leave = std::stoi(inputString2.at(0)0;
-                        String animal = inputString1.at(0);
+                        int leave = std::stoi(inputString2.at(0));
+                        char animal = inputString1.at(0);
 
                         if (animal == "D") {
-                                people.add(people.begin(), new Person(keep, -leave));
+								Person person;
+								person.setValues(k, -keep, leave);
+                                people.add(people.begin(), person);
                         }
                         else {
-                                people.add(people.begin(), new Person(-keep, leave));
+								Person person;
+								person.setValues(k, -keep, leave);
+                                people.add(people.begin(), person);
                         }
 
                 }
+                
+				// making map
+                for (Person person1 : people) {
+                        for (Person person2 : people) {
+                                if ((person1.keep == person2.leave) && (person1 != person2)) {
+									map.insert({person1.key, true});
+									map.insert({person2.key, true});
+								}
+								else if (person2.keep == person1.leave && person1 != person2) {
+									map.insert({person1.key, true});
+									map.insert({person2.key, true});
+								}
+						}
 
-                for (int person1 : people) {
-                        for (int person2 : people) {
-                                if (person1.keep == person2.leave && person1 != person2) {
                 }
-
-                }
+                
+                for (int n = 0; n < voters; n++){
+					matching[n] = -1;
+					
+					vector <bool> checked;
+					int count = 0;
+					for (int x = 0; x < voters; x++){
+						if (people.get(x).keep < 0) {
+							if (match(x, checked)){
+								count++;
+							}
+						}
+					}
+				}
+				
+				cout << voters-count;
 
         }
+}
+
+void match(int p; vector <bool> checked) {
+	for (int adj = 0; adj < voters; adj++){
+		if ((adj = map.at(p)) && (adj != p)) {
+			if (matching[adj] < 0){
+				matching[adj] = p;
+				return true
+			}
+		}
+		
+		if ((adj = map.at(p)) && !checked[adj] && (adj != p)){
+			checked[adj] = true;
+			if (match(matching[adj], checked)){
+				matching[adj] = p;
+				return true;
+			}
+		}
+	}
+	return false;
 }
